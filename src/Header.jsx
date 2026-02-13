ce3d0435390ccaab8ca5941f9c12c1cfd2d86d39
Andre Ugolini <andreugolini@unochapeco.edu.br>
2026-02-13 17:42:04 -0300

    Corrigir cores do header.

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,5 +1,4 @@
 import { Stack, Typography } from "@mui/material";
-import { FormControlLabel } from "@mui/material";
 import { CustomSwitch } from "./CustomSwitch";
 
 export default function Header({ darkMode, setDarkMode }) {
@@ -7,12 +6,17 @@ export default function Header({ darkMode, setDarkMode }) {
     <>
       <Stack bgcolor='background.header' height={300} width="100%" justifyContent="center" alignItems="center">
         <Stack width="80%" direction={{ xs: "column", sm: "row" }} justifyContent="space-between">
-          <Typography variant="h4" fontWeight="bold">
+          <Typography variant="h3" fontWeight="bold">
             Social Media Dashboard
           </Typography>
           <Stack alignItems="end">
             <CustomSwitch onChange={() => setDarkMode(!darkMode)}></CustomSwitch>
           </Stack>
+        </Stack>
+        <Stack width="80%">
+          <Typography variant="h6" fontWeight="bold" sx={{ color: "hsl(228, 12%, 44%)" }}>
+            Total Followers: 23,004
+          </Typography>
         </Stack>
       </Stack>
     </>
